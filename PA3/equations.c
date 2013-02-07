b3c890752c6866f29c51c6413d7eca18b4637c82
Jacob Wright <jacob.t.wright@email.wsu.edu>
2013-02-06 16:13:30 -0800

    Attempt 1

---
+++ PA3/equations.c
@@ -0,0 +1,82 @@
+/*******************************************************************************************
+* Programmer: Jacob Wright                                                                 *
+* Class: Cpt S 121, Spring 2013; Lab Section 4                                             *
+* Programming Assignment 3:  Statistical Analysis of Student Records                       *
+* Date: February 6, 2013                                                                   *
+* Description:  File opening, reading, writing and fuctions                                *
+*******************************************************************************************/
+
+#include "equations.h"
+
+//Reads one double precision number from the input file
+double read_double (FILE *infile)
+{
+	double number = 0;
+	fscanf (infile, "%.2lf", &number);
+	if (number != 0.0)
+	{
+		printf ("Number = %.2lf \n", number);
+	}
+	return number;
+}
+
+//Reads one integer number from the input file
+int read_integer (FILE *infile)
+{
+	double number = 0;
+	fscanf (infile, "%d", &number);
+	if (number != NULL)
+	{
+		printf ("Number = %d \n", number);
+	}
+	return number;
+}
+
+//Finds the sum of number1, number2, number3, number4, and number5 and returns the result
+double calculate_sum (double number1, double number2, double number3, double number4, double number5)
+{
+
+}
+
+//Determines the mean through the calculation sum / number and returns the result
+double calculate_mean (double sum, int number)
+{
+
+}
+
+//Determines the deviation of number from the mean and returns the result
+double calculate_deviation (double number, double mean)
+{
+
+}
+
+//Determines the variance through the calculation: ((deviation1)^2 + (deviation2)^2 + (deviation3)^2 + (deviation4)^2 + (deviation5)^2) / number and returns the result
+//Call calculate_mean
+double calculate_variance (double deviation1, double deviation2, double deviation3, double deviation4, double deviation5, int number)
+{
+
+}
+
+//Calculates the standard deviation as sqrt (variance) and returns the result
+double calculate_standard_deviation (double variance)
+{
+
+}
+
+//Determines the maximum number out of the five input parameters passed into the function, returning the max
+double find_max (double number1, double number2, double number3, double number4, double number5)
+{
+
+}
+
+//Determines the minimum number out of the five input parameters passed into the function, returning the min
+double find_min (double number1, double number2, double number3, double number4, double number5)
+{
+
+}
+
+//Prints a double precision number (to the hundredths place) to an output file
+void print_double (FILE *outfile, double number)
+{
+
+}
---
+++ PA3/main.c
@@ -0,0 +1,48 @@
+/*******************************************************************************************
+* Programmer: Jacob Wright                                                                 *
+* Class: Cpt S 121, Spring 2013; Lab Section 4                                             *
+* Programming Assignment 3:  Statistical Analysis of Student Records                       *
+* Date: February 6, 2013                                                                   *
+* Description:  File opening, reading, writing and fuctions                                *
+*******************************************************************************************/
+
+#include "equations.h"
+
+int main (void)
+{
+	FILE *infile = NULL, *outfile = NULL;
+	double read_record = 0, double_number = 0;
+	int int_number = 0;
+	//Opens an input file "input.dat" for reading;
+	infile = fopen ("input.dat", "r");
+    //Opens an output file "output.dat" for writing;
+	outfile = fopen ("output.dat", "w");
+	//Reads five records from the input file (input.dat); You will need to use a combination of read_double ( ) and read_integer ( ) function calls here!
+	read_record = read_double (infile);
+	read_record = read_integer (infile);
+	
+
+
+
+    //Calculates the sum of the GPAs;
+    //Calculates the sum of the class standings;
+    //Calculates the sum of the ages;
+ 
+    //Calculates the mean of the GPAs, writing the result to the output file (output.dat);
+    //Calculates the mean of the class standings, writing the result to the output file (output.dat);
+    //Calculates the mean of the ages, writing the result to the output file (output.dat);
+ 
+    //Calculates the deviation of each GPA from the mean (Hint: need to call calculate_deviation ( ) 5 times)
+    //Calculates the variance of the GPAs
+    //Calculates the standard deviation of the GPAs, writing the result to the output file (output.dat);
+ 
+    //Determines the min of the GPAs, writing the result to the output file (output.dat);
+    //Determines the max of the GPAs, writing the result to the output file (output.dat);
+ 
+    //Closes the input and output files (i.e. input.dat and output.dat)
+
+	
+
+	return 0;
+}
+
